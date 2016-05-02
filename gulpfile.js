90557a59faeb17748cf2a0eb3d747bd55b238067
Daniel Ochoa John <dochoaj@gmail.com>
2016-05-01 21:56:38 -0300

    Add boilerplate

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+var gulp = require('gulp');
+var del = require('del')
+var gutil = require('gulp-util');
+
+var concat = require('gulp-concat');
+var haml = require('gulp-haml');
+var gulpCopy = require('gulp-copy');
+var source = require('vinyl-source-stream');
+
+// var coffee = require('gulp-coffee');
+
+var babel = require('gulp-babel');
+var browserify = require('browserify');
+var babelify = require('babelify');
+
+
+gulp.task('clean', function() {
+    return del(['./build']);
+});
+
+gulp.task('scripts', ['clean'], function() {
+  return gulp.src('./app/scripts/**/*.jsx')
+    .pipe(concat('app.jsx'))
+    .pipe(gulp.dest('./app'));
+});
+
+// gulp.task('coffee', ['scripts'], function() {
+//   return gulp.src('./app/app.coffee')
+//     .pipe(coffee({bare: true}).on('error', gutil.log))
+//     .pipe(gulp.dest('./build'));
+// });
+
+gulp.task('babel', ['clean'], function() {
+  return browserify({entries: './app/scripts/app.jsx', extensions: ['.jsx'], debug: true})
+    .transform('babelify', {presets: ['react', 'es2016']})
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('templates', ['clean'], function() {
+  return gulp.src('./app/index.haml')
+    .pipe(haml({
+      compiler: 'visionmedia'
+    }))
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('styles', ['clean'], function() {
+  return gulp.src('./app/styles/app.css')
+    .pipe(gulpCopy(
+      './build', { prefix: 1 }
+    ));
+});
+
+gulp.task('build', ['clean', 'babel', 'templates', 'styles']);
